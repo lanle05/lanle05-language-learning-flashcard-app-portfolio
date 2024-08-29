aefef615a8e0eaa993efd207cf8cf7003600dbbd
Omobolanle <omobolanle.shaibu@gmail.com>
2024-08-29 14:18:45 +0100

    Sign Up error fix

--- src/Signup_page.js
+++ src/Signup_page.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"; // Ensure useState is imported
+import React, { useState } from "react";
 import {
   createUserWithEmailAndPassword,
   signInWithPopup,
@@ -12,38 +12,22 @@ import { FcGoogle } from "react-icons/fc";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState(""); // Define the error state
   const navigate = useNavigate();
 
   const handleSignup = async (e) => {
     e.preventDefault();
-    setError(""); // Reset error message
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       navigate("/dashboard"); // Redirect to dashboard on successful signup
     } catch (error) {
-      switch (error.code) {
-        case "auth/email-already-in-use":
-          setError("This email is already in use. Please log in.");
-          break;
-        case "auth/invalid-email":
-          setError("Invalid email format.");
-          break;
-        case "auth/weak-password":
-          setError("Password is too weak. Please use a stronger password.");
-          break;
-        default:
-          setError("An unexpected error occurred. Please try again.");
-      }
+      console.error("Error signing up:", error);
     }
   };
 
   const handleGoogleSignUp = async () => {
     const provider = new GoogleAuthProvider();
     try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      console.log("User:", user);
+      await signInWithPopup(auth, provider);
       navigate("/dashboard"); // Redirect to dashboard on successful signup
     } catch (error) {
       console.error("Error with Google sign-up:", error);
@@ -70,8 +54,6 @@ const Signup = () => {
         />
         <button type="submit">Sign Up</button>
       </form>
-      {error && <p className="error-message">{error}</p>}{" "}
-      {/* Display error message */}
       <button onClick={handleGoogleSignUp} className="google-auth-button">
         <FcGoogle />
         Sign Up with Google
